332cfb0c04aa08d7e20c00dac30eb1719f7eafa9
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2024-05-14 21:36:01 -0300

    Update navBar.vue

--- hci-tp-web/src/components/navBar.vue
+++ hci-tp-web/src/components/navBar.vue
@@ -5,7 +5,7 @@
         v-model="drawer"
         temporary
       >
-        <v-list-item
+        <v-list-item class="drawer_header"
           prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
           title="CONNECTIT"
         ></v-list-item>
@@ -13,28 +13,28 @@
         <v-divider></v-divider>
 
         <v-list nav class="drawer_button">
-          <v-list-item class="drawer_elem" value="rooms">
+          <v-list-item class="drawer_elem" @click="navigateTo('devices')">
             <template v-slot:prepend>
               <v-icon class="icon">mdi-devices</v-icon> 
             </template>
             <v-list-item-title class="text">DEVICES</v-list-item-title> 
           </v-list-item>
 
-          <v-list-item class="drawer_elem" value="rooms">
+          <v-list-item class="drawer_elem" @click="navigateTo('rooms')">
             <template v-slot:prepend>
               <v-icon class="icon">mdi-home</v-icon> 
             </template>
             <v-list-item-title class="text">ROOMS</v-list-item-title> 
           </v-list-item>
 
-          <v-list-item class="drawer_elem" value="rooms">
+          <v-list-item  class="drawer_elem" @click="navigateTo('routines')">
             <template v-slot:prepend>
               <v-icon class="icon">mdi-day</v-icon> 
             </template>
             <v-list-item-title class="text">ROUTINES</v-list-item-title> 
           </v-list-item>
 
-          <v-list-item class="drawer_elem" value="rooms">
+          <v-list-item class="drawer_elem" @click="navigateTo('users')">
             <template v-slot:prepend>
               <v-icon class="icon">mdi-account-multiple</v-icon> 
             </template>
@@ -65,32 +65,35 @@ export default {
   methods: {
     toggleDrawer() {
       this.drawer = !this.drawer; // toggle drawer state
+    },
+    navigateTo(page) {
+      // Perform navigation based on the selected page
+      console.log(`Navigating to ${page}`);
     }
   }
 };
 </script>
 
 <style scoped>
-  .drawer{
+  .drawer {
     background: rgb(var(--v-theme-primary));
   }
-  .drawer_button{
+  .drawer_button {
     color: rgb(var(--v-theme-tertiary));
   }
-  .drawer_elem{
+  .drawer_elem {
     background: rgb(var(--v-theme-primary_v));
     color: rgb(var(--v-theme-primary));
-    
+  }
+  .drawer_header_text{
+    color: rgb(var(--v-theme-primary_v));
   }
   /* Scoped styles for the text */
   .drawer_elem .text {
-    font-size: 20px; /* Change text font size */
+    font-size: 1rem; /* Change text font size */
   }
-
   /* Scoped styles for the icons */
   .drawer_elem .icon {
-    font-size: 35px; /* Change icon font size */
+    font-size: 2rem; /* Change icon font size */
   }
-
-
 </style>
