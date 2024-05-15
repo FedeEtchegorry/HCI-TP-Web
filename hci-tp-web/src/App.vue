1445ce7d0acf88d9ca707f29d5e7cecbed781b14
ivanOdz <miodzomek@gmail.com>
2024-05-15 00:56:38 -0300

    Update App.vue

--- hci-tp-web/src/App.vue
+++ hci-tp-web/src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <v-app>
-    <!--Primer Pagina -->
-    <div class="d-flex flex-column"> 
-    <v-main >
-      <div class="d-flex">
-        <NavigationBar/>
-        <div class="flex-grow-1 custom-outer-canvas-div">
-        <div >
-          <SearchComponent class="mt-2 mb-4"/>
-        </div>
-        <div class="d-grid custom-inner-canvas-div">
-          <!--CANVA-->
-        </div>
+      <!--Primer Pagina -->
+      <div class="d-flex flex-column">
+        <v-main>
+          <div class="d-flex">
+            <NavigationBar />
+            <div class="flex-grow-1 custom-outer-canvas-div custom-div">
+              <div>
+                <SearchComponent class="mt-2 mb-4" />
+              </div>
+              <div class="d-grid custom-inner-canvas-div">
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </v-main>
       </div>
+      <!--Segunda Pagina -->
+      <div>
+        <v-main>
+        </v-main>
       </div>
-    </v-main>
-  </div>
-  <!--Segunda Pagina -->
-  <div> 
-    <v-main>
-    </v-main>
-  </div>
   </v-app>
 </template>
 
@@ -28,14 +28,19 @@
 import NavigationBar from './components/NavigationBar.vue';
 import SearchComponent from './components/SearchComponent.vue';
 
-  //
+//
 </script>
 
 <style scoped>
 .custom-outer-canvas-div {
   background-color: rgb(var(--v-theme-secondary));
 }
+
 .custom-inner-canvas-div {
   background-color: rgb(var(--v-theme-secondary-v));
+}
+
+.custom-div {
+  height: 50%; 
 }
 </style>
