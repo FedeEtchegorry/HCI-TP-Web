254060eca6ead47d8ebb2bc17542bf46b8651aee
Lucas Campoli <lcampoli@itba.edu.ar>
2024-05-19 17:27:47 -0300

    Empty card

---
+++ hci-tp-web/src/components/CardDetail/EmptyCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card class="fixed-card">
+        <slot></slot>
+    </v-card>
+</template>
+
+<script setup>
+const status = defineModel('status');
+</script>
+
+<style scoped>
+
+.fixed-card {
+    height: 15rem;
+    /* Ajusta según tus necesidades */
+    width: 100%;
+    border: 3px solid;
+}
+</style>
