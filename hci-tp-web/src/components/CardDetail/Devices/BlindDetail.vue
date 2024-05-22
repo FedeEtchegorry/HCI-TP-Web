ba7dbee35d2e32f90228cba22c846e2b5203b153
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2024-05-22 03:01:20 -0300

    blind working almost right

--- hci-tp-web/src/components/CardDetail/Devices/BlindDetail.vue
+++ hci-tp-web/src/components/CardDetail/Devices/BlindDetail.vue
@@ -1,24 +1,36 @@
 <template>
-    <v-card width="15rem">
-        <v-slider class="blind" 
-            v-model="openPercentage"
-            append-icon="mdi-blinds-open"
-            prepend-icon="mdi-roller-shade-closed"
-            @click:append="openBlind"
-            @click:prepend="closeBlind"
-            step="1"
-            min="0"
-            max="100"
-            track-fill-color="blue_state"
-            track-color="grey"
-            track-size="4"
-            thumb-label="always"
-            thumb-color="blue_state"
-            reverse
-        ></v-slider>
-        <p> {{percentageClosed}}%</p>
+    <v-card class="device">
+            <v-card class="box">
+            <p v-if="isOpening" class="align-center title">ABIERTO</p>
+            <p v-if="isClosing" class="align-center title">CERRADO</p>
+            </v-card>
+      <v-container fluid class="mb-5">
+            <v-slider class="blind" 
+                v-model="openPercentage"
+                step="1"
+                min="0"
+                max="100"
+                track-fill-color="blue_state"
+                track-color="grey"
+                track-size="6"
+                thumb-label="always"
+                thumb-color="blue_state"
+                height="50" elevation="5"
+            ></v-slider>
+      </v-container>
+        <v-row justify="space-around">
+          <v-btn @click="openBlind" :disabled="isOpening" color="green" rounded elevation="5">
+            <v-icon class="icon mr-2">mdi-blinds-open</v-icon>
+            Abrir
+          </v-btn>
+          <v-btn @click="closeBlind" :disabled="isClosing" color="red" rounded elevation="5">
+            <v-icon class="icon mr-2">mdi-roller-shade-closed</v-icon>
+            Cerrar
+          </v-btn>
+        </v-row>
+      
     </v-card>
-</template>
+  </template>
 
 <script setup>
 import { ref, computed, watch, onBeforeMount } from 'vue';
@@ -26,7 +38,9 @@ import { useRoomStore } from '@/stores/roomStore';
 import { useDeviceStore } from '@/stores/deviceStore';
 
 
-
+onBeforeMount(() => {
+    openPercentage.value = myDevice.value.state.level;
+});
 
 const openPercentage = ref(0);
 
@@ -55,19 +69,19 @@ watch(openPercentage, (newValue) => {
 });
 
 async function setPosition(value) {
-    if (value >= 0 && value <= 100) {
-        try {await deviceStore.execute(deviceId.value, 'setLevel', [value]);
-        openPercentage.value = value;
-        console.log(`Open ${openPercentage.value}%`);}
+        try {
+        await deviceStore.execute(deviceId.value, 'setLevel', [value]);
+            openPercentage.value = value;
+            console.log(`Open ${openPercentage.value}%`);}
         catch(e){
-        console.log(e);
-        }   
-    }
+            console.log(e);
+        }
 }
 
 async function openBlind() {
-    try {await deviceStore.execute(deviceId.value, 'open', []);
-        openPercentage.value = 0;
+    try {
+    await deviceStore.execute(deviceId.value, 'open', []);
+        myDevice.value.state.status='opening';
     }
     catch(e){
         console.log(e);
@@ -75,8 +89,9 @@ async function openBlind() {
 }
 
 async function closeBlind() {
-    try {await deviceStore.execute(deviceId.value, 'close', []);
-        openPercentage.value = 100;
+    try {
+    await deviceStore.execute(deviceId.value, 'close', []);
+        myDevice.value.state.status='closing';
     }
     catch(e){
         console.log(e);
@@ -87,15 +102,31 @@ async function closeBlind() {
 </script>
 
 <style scoped>
-.blind {
-    color: rgb(var(--v-theme-primary));
-    background-color: rgb(var(--v-theme-primary_v));
-    accent-color: red;
-    margin-top: 2rem;
+.device {
+    padding: 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    flex-grow: 1;
 }
-.open-value{
-    justify-content: center;
-    align-self: center;
-    font-size: medium;
+
+.box{
+    background-color: rgb(var(--v-theme-blue_state));
+    border-color: black;
+    border-width: 0.2rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    margin-bottom: 2rem;
 }
+
+.title{
+    font-size: xx-large;
+    font-weight: 500;
+    color: white;
+    background-color: rgb(var(--v-theme-blue_state));
+}
+
+
+
 </style>
