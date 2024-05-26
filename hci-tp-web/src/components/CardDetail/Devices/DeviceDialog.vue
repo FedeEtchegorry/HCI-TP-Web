5ac7edc32bcd46bc0f68d0aef48daa88e52bff00
Lucas Campoli <lcampoli@itba.edu.ar>
2024-05-26 13:17:35 -0300

    devicedialog

--- hci-tp-web/src/components/CardDetail/Devices/DeviceDialog.vue
+++ hci-tp-web/src/components/CardDetail/Devices/DeviceDialog.vue
@@ -1,8 +1,22 @@
 <template>
-  <v-btn @click="showCard = true" :prepend-icon="device.meta.logo">Device {{ device.room?.name }} </v-btn>
-  <v-dialog v-model="showCard" max-width="40%" height="40%">
+  <v-card color="primary_v">
+    <v-card @click="showCard = true" class="ma-4" outlined>
+      <v-card-title><v-icon>{{ device.meta.logo }}</v-icon> {{ device.name }}</v-card-title>
+      <v-card-subtitle>Location: {{ device.room.name }} </v-card-subtitle>
+      <v-card-text>
+        <div><strong>Consumo de Energía:</strong> {{ device.type.powerUsage }}W</div>
+        <div v-if="device.state.batteryLevel"><strong>Nivel de Batería:</strong> {{ device.state.batteryLevel }}%</div>
+        <div v-if="device.state.status"><strong>Estado:</strong> {{ capitalizeFirstLetter(device.state.status) }}</div>
+        <div v-if="device.state.mode"><strong>Modo:</strong> {{ capitalizeFirstLetter(device.state.mode) }}</div>
+      </v-card-text>
+    </v-card>
+
+
+    <v-dialog v-model="showCard" max-width="40%" height="40%">
       <DeviceDetail :device="device" v-model="showCard"></DeviceDetail>
-  </v-dialog>
+    </v-dialog>
+  </v-card>
+
 </template>
 
 <script setup>
@@ -14,6 +28,14 @@ const props = defineProps({
   device: Object,
 });
 
+function capitalizeFirstLetter(string) {
+  if (!string) 
+    return string; 
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 let showCard = ref(false);
 
 </script>
+
+<style scoped></style>
